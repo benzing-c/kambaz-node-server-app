e6b73a378be4b0ba952c655801d177b1048404cf
Casey Benzing <benzing.c@northeastern.edu>
2025-04-16 20:42:42 -0400

    a6 bad wip

--- Kambaz/Enrollments/dao.js
+++ Kambaz/Enrollments/dao.js
@@ -1,17 +1,37 @@
-import Database from "../Database/index.js";
-import { v4 as uuidv4 } from "uuid";
+// import Database from "../Database/index.js";
+// import { v4 as uuidv4 } from "uuid";
 
-export function enrollUserInCourse(userId, courseId) {
-    const { enrollments } = Database;
-    enrollments.push({ _id: uuidv4(), user: userId, course: courseId });
+// export function enrollUserInCourse(userId, courseId) {
+//     const { enrollments } = Database;
+//     enrollments.push({ _id: uuidv4(), user: userId, course: courseId });
+// }
+
+// export function deleteEnrollment(userId, courseId) {
+//     const { enrollments } = Database;
+//     Database.enrollments = enrollments.filter((enrollment) => enrollment.user !== userId || enrollment.course !== courseId);
+// }
+
+// export function getAllEnrollments() {
+//     const { enrollments } = Database;
+//     return enrollments;
+// }
+
+import model from "./model.js";
+
+export async function findCoursesForUser(userId) {
+    const enrollments = await model.find({ user: userId }).populate("course");
+    console.log("enroollments");
+    console.log(userId);
+    console.log(enrollments);
+    return enrollments.map((enrollment) => enrollment.course);
 }
-
-export function deleteEnrollment(userId, courseId) {
-    const { enrollments } = Database;
-    Database.enrollments = enrollments.filter((enrollment) => enrollment.user !== userId || enrollment.course !== courseId);
+export async function findUsersForCourse(courseId) {
+    const enrollments = await model.find({ course: courseId }).populate("user");
+    return enrollments.map((enrollment) => enrollment.user);
 }
-
-export function getAllEnrollments() {
-    const { enrollments } = Database;
-    return enrollments;
+export function enrollUserInCourse(user, course) {
+    return model.create({ user, course, _id: `${user}-${course}` });
+}
+export function unenrollUserFromCourse(user, course) {
+    return model.deleteOne({ user, course });
 }
